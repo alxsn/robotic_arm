1dd17f791280588996da9bbcac40380744565064
alex <als01bm@gmail.com>
2025-08-26 17:34:54 -0300

    sensor magnetico

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,21 @@
 #include <Servo.h>
+#include <QMC5883LCompass.h>
+
+QMC5883LCompass compass;
 
 Servo servo1, servo2, servo3, servo4, servo5, servo6;
 String input = "";
 
 void setup() {
   Serial.begin(9600);
-  servo1.attach(9);   // Conecte o servo 1 ao pino 9
-  servo2.attach(10);  // Conecte o servo 2 ao pino 10
-  servo3.attach(11);  // Conecte o servo 3 ao pino 11
-  servo4.attach(3);
-  servo5.attach(5);
-  servo6.attach(6);
+  servo1.attach(3);   // 
+  servo2.attach(5);  // dourado
+  servo3.attach(6);  // verde
+  servo4.attach(9);//laranja com corretivo
+  servo5.attach(10);//roxo
+  servo6.attach(11);//amarelo
+
+  compass.init();
 
   //servo4.attach(3, 500, 2500);
   //servo4.writeMicroseconds(1500);  // posição neutra (~90°)
@@ -25,9 +30,19 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
+    compass.read();
+
+    int x = compass.getX();
+    int y = compass.getY();
+    int z = compass.getZ();
+    int heading = compass.getAzimuth();
+
     input = Serial.readStringUntil('\n');
     int s1, s2, s3, s4, s5, s6;
     sscanf(input.c_str(), "%d,%d,%d,%d,%d,%d", &s1, &s2, &s3, &s4, &s5, &s6);
+    // if(z < 17 && z > -17){
+    //   s1=70;
+    // }
     servo1.write(s1);
     servo2.write(s2);
     servo3.write(s3);
